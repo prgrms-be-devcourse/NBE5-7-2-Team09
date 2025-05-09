d4a1a0f8c1eb63b9c8af226993b3b0c9bc8b5379
kimsj0970 <53886275+kimsj0970@users.noreply.github.com>
2025-05-09 12:31:12 +0900

    fix: security 로그아웃 수정 (#5)

--- Readio/src/main/java/ninegle/Readio/admin/comfig/SecurityConfig.java
+++ Readio/src/main/java/ninegle/Readio/admin/comfig/SecurityConfig.java
@@ -1,10 +1,12 @@
 package ninegle.Readio.admin.comfig;
 
+import lombok.RequiredArgsConstructor;
 import ninegle.Readio.admin.Repository.BlackListRepository;
 import ninegle.Readio.admin.app.UserService;
 import ninegle.Readio.admin.app.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -16,6 +18,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
@@ -45,14 +48,13 @@ public class SecurityConfig {
 
                 .httpBasic(httpBasic->httpBasic.disable())
                 .authorizeHttpRequests(auth ->{auth
-                        .requestMatchers("/login","/signup","/logout").permitAll()
+                        .requestMatchers("/user/login","/user/signup","/user/logout").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().hasAnyRole("USER", "ADMIN"); //나머지 요청은 USER나 ADMiN 권한을 가져야 접근 가능
                 })
 
                 .sessionManagement(session->session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
@@ -61,5 +63,8 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
+
+
 
 }
