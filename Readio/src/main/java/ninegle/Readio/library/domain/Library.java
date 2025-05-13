cbe105057ac6d1ab1cd4da68f717fcf65aa2750c
kimsj0970 <kimsj0970@naver.com>
2025-05-13 10:55:13 +0900

    fix: 오류 수정

--- Readio/src/main/java/ninegle/Readio/library/domain/Library.java
+++ Readio/src/main/java/ninegle/Readio/library/domain/Library.java
@@ -4,12 +4,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -24,7 +21,6 @@ import ninegle.Readio.user.domain.User;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EntityListeners(AuditingEntityListener.class)
 public class Library {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -40,13 +36,13 @@ public class Library {
 	@OneToMany(mappedBy = "library", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<LibraryBook> libraryBook;
 
-	@CreatedDate
 	private LocalDateTime createdAt;
 
 	@UpdateTimestamp
 	private LocalDateTime updatedAt;
 
 	public Library(String libraryname, User user) {
+		this.createdAt = LocalDateTime.now();
 		this.libraryName = libraryname;
 		this.user = user;
 	}
